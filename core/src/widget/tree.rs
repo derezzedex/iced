abd2a8679ef6edcee00da3171fe4ea8048463711
Richard Custodio <richardcustodio@pm.me>
2025-07-24 00:42:06 -0300

    add `track_caller` to `State::downcast` methods

--- core/src/widget/tree.rs
+++ core/src/widget/tree.rs
@@ -230,6 +230,7 @@ impl State {
     ///
     /// # Panics
     /// This method will panic if the downcast fails or the [`State`] is [`State::None`].
+    #[track_caller]
     pub fn downcast_ref<T>(&self) -> &T
     where
         T: 'static,
@@ -246,6 +247,7 @@ impl State {
     ///
     /// # Panics
     /// This method will panic if the downcast fails or the [`State`] is [`State::None`].
+    #[track_caller]
     pub fn downcast_mut<T>(&mut self) -> &mut T
     where
         T: 'static,
