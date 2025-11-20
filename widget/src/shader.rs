2300281a43d7dffd935b0f07bc2c4c939f794288
Richard Custodio <richardcustodio@pm.me>
2025-11-19 22:22:03 -0300

    re-export `primitive::Renderer` on `shader` shader

--- widget/src/shader.rs
+++ widget/src/shader.rs
@@ -16,7 +16,7 @@ use std::marker::PhantomData;
 
 pub use crate::Action;
 pub use crate::graphics::Viewport;
-pub use primitive::{Primitive, Storage};
+pub use primitive::{Primitive, Renderer, Storage};
 
 /// A widget which can render custom shaders with Iced's `wgpu` backend.
 ///
